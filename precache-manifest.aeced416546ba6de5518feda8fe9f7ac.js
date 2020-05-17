3cc0f6107d1d83897c7338e8ee9afec0c09e1029
khrystyna.styslo <khrystyna.styslo@otakoyi.com.ua>
2020-05-17 04:15:11 +0300

    Updates

---
+++ precache-manifest.aeced416546ba6de5518feda8fe9f7ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5722227bfa4a297ad816d98788247ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a25205349a1e6fc163b6",
+    "url": "/static/css/main.40776cf5.chunk.css"
+  },
+  {
+    "revision": "e77a69bfef11986c7c8b",
+    "url": "/static/js/2.d20e114e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.d20e114e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a25205349a1e6fc163b6",
+    "url": "/static/js/main.08d1d170.chunk.js"
+  },
+  {
+    "revision": "68b4a1c9ec117ca1949b",
+    "url": "/static/js/runtime-main.9fa52d75.js"
+  }
+]);
