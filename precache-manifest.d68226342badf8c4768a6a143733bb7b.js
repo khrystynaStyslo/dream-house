561ad5a0dc6be3e2ea526d079de4aa0029abeaaa
khrystyna.styslo <khrystyna.styslo@otakoyi.com.ua>
2020-05-17 20:04:50 +0300

    Updates

---
+++ precache-manifest.d68226342badf8c4768a6a143733bb7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99fb916d9e1f0378dc9292caf6d931d3",
+    "url": "/dream-house/index.html"
+  },
+  {
+    "revision": "a25205349a1e6fc163b6",
+    "url": "/dream-house/static/css/main.40776cf5.chunk.css"
+  },
+  {
+    "revision": "e77a69bfef11986c7c8b",
+    "url": "/dream-house/static/js/2.d20e114e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/dream-house/static/js/2.d20e114e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a25205349a1e6fc163b6",
+    "url": "/dream-house/static/js/main.08d1d170.chunk.js"
+  },
+  {
+    "revision": "d29f4b8ebab5ab2c5695",
+    "url": "/dream-house/static/js/runtime-main.80cff233.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aeced416546ba6de5518feda8fe9f7ac.js"
+  "/dream-house/precache-manifest.d68226342badf8c4768a6a143733bb7b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dream-house/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
